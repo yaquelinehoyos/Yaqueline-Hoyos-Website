d1470390945432b5ddc2d974140de0cf87c57609
yaquelinehoyos <yaqueline150@gmail.com>
2021-12-30 18:59:12 -0500

    :sparkles: create social media buttons

--- src/components/socialMedia/SocialMedia.tsx
+++ src/components/socialMedia/SocialMedia.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './socialMedia.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function SocialMedia() {
   return (
-    <div className="Info">
-      <p>Social Media Component</p>
+    <div className="social-media">
+        <div className="social-media__icon">
+            <a href="https://github.com/yaquelinehoyos" target="_blank">
+                <i className="fab fa-github-alt fa-2x"></i>
+            </a>
+        </div>
+        <div className="social-media__icon">
+            <a href="https://www.linkedin.com/in/yaquelinehoyos/" target="_blank">
+                <i className="fab fa-linkedin-in fa-2x"></i>
+            </a>
+        </div>
     </div>
   );
 }

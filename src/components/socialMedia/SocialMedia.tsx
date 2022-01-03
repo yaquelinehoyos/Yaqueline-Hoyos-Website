988d24d90c656f93503d0521388a5060513a0071
yaquelinehoyos <yaqueline150@gmail.com>
2022-01-03 12:23:16 -0500

    :ambulance: fix security risk in target='_blank' link

--- src/components/socialMedia/SocialMedia.tsx
+++ src/components/socialMedia/SocialMedia.tsx
@@ -6,12 +6,12 @@ function SocialMedia() {
   return (
     <div className="social-media">
         <div className="social-media__icon">
-            <a href="https://github.com/yaquelinehoyos" target="_blank" rel="noreferrer">
+            <a href="https://github.com/yaquelinehoyos" target="_blank" rel="noopener noreferrer">
                 <i className="fab fa-github-alt fa-2x"></i>
             </a>
         </div>
         <div className="social-media__icon">
-            <a href="https://www.linkedin.com/in/yaquelinehoyos/" target="_blank" rel="noreferrer">
+            <a href="https://www.linkedin.com/in/yaquelinehoyos/" target="_blank" rel="noopener noreferrer">
                 <i className="fab fa-linkedin-in fa-2x"></i>
             </a>
         </div>
